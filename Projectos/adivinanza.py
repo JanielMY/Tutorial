5763ee2017cf928ff67edbf5cc472964e78daf08
Samuel Hernandez <samuel.hernandez@wallawalla.edu>
2023-07-27 18:50:47 -0600

    Incluir proyecto

---
+++ Projectos/adivinanza.py
@@ -0,0 +1,15 @@
+import random
+
+"""
+Adivina el numero
+
+Projecto: crear un programa que pida al usuario adivinar un numero entre 1 y n, donde el usario elige n. 
+"""
+
+# Pedirle al usuario rango de numeros para adivinar
+n = input("Ingresa rango de numeros por adivinar: ")
+number_to_guess = random.randint(1, n)
+
+# Pedirle al usuario numero por adivinar
+guess = input("Ingresa tu numero por adivinar")
+
