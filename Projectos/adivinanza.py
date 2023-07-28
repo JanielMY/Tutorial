dcc4c67d3162f22ad66db2f117f73f3233454c11
Profesor Flex <profesorflex@192.168.1.12>
2023-07-27 19:12:31 -0600

    adivinanza

--- Projectos/adivinanza.py
+++ Projectos/adivinanza.py
@@ -7,9 +7,24 @@ Projecto: crear un programa que pida al usuario adivinar un numero entre 1 y n, 
 """
 
 # Pedirle al usuario rango de numeros para adivinar
-n = int(input("Ingresa rango de numeros por adivinar: "))
+n = int (input("Ingresa rango de numeros por adivinar: "))
 number_to_guess = random.randint(1, n)
 
-# Pedirle al usuario numero por adivinar
-guess = input("Ingresa tu numero por adivinar: ")
 
+
+
+not_guessed = True 
+while not_guessed:
+    # Pedirle al usuario numero por adivinar
+    guess = int (input("Ingresa tu numero por adivinar: "))
+
+    # adivino muy abajo
+    if guess < number_to_guess:
+        print ("adivino muy abajo")
+    # adivino muy arriba
+    if guess > number_to_guess:
+        print ("adivino muy arriba")
+    # adivino bien
+    if guess == number_to_guess:
+        print ("adivino bien")
+        not_guessed = False
